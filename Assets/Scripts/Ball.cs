e66d0b4024f642505d7ea217051b85b7ab2cd6c4
Moose Hole <moose_hole@yahoo.com>
2020-07-11 16:14:34 -0500

    Ball self destruction

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	public bool OneSecondToSelfDestruct = false;
+	private int selfDestructSeconds = 5;
+	private int stationarySeconds = 0;
+	private Vector3 _currentPosition;
+	private Vector3 _previousPosition;
+	bool timerStarted = false;
+	
+	void Start()
+	{
+		_currentPosition = this.GetComponent<Transform>().position;
+		_previousPosition = _currentPosition;
+		Debug.Log("-=-=- start");
+		
+		if (!timerStarted)
+		{
+			timerStarted = true;
+			Invoke ( "_tick", 1f );
+		}
+	}
+
+	private void Update()
+	{
+		_currentPosition = this.GetComponent<Transform>().position;
+
+		if (_previousPosition != _currentPosition)
+		{
+			OneSecondToSelfDestruct = false;
+			stationarySeconds = 0;
+			Debug.Log("-=-=- .");
+		}
+		else if (stationarySeconds >= selfDestructSeconds - 1)
+		{
+			OneSecondToSelfDestruct = true;
+			Debug.Log("-=-=- !!@");
+		}
+		else
+		{
+			Debug.Log("-=-=- !");
+		}
+		
+		_previousPosition = _currentPosition;
+		
+		if (stationarySeconds >= selfDestructSeconds)
+		{
+			Debug.Log("-=-=- KABOOM!");
+		}
+	}
+	
+	private void _tick()
+	{
+		stationarySeconds++;
+		Invoke ( "_tick", 1f );
+	}
+}
