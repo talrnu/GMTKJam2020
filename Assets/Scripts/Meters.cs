1616abe16e6180460add219bb95e48e646c9bde4
Moose Hole <moose_hole@yahoo.com>
2020-07-11 15:35:46 -0500

    Add points meter based on timer

---
+++ Assets/Scripts/Meters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Meters : MonoBehaviour
+{
+	private int points = 0;
+	bool timerStarted = false;
+	private Text meterPoints;
+	
+	void Start()
+	{
+		meterPoints = GameObject.Find("MeterPoints").GetComponent<Text>();
+		meterPoints.text = "Roll out";
+		
+		if (!timerStarted)
+		{
+			timerStarted = true;
+			Invoke ( "_tick", 1f );
+		}
+	}
+
+	void OnGUI ()
+	{
+		meterPoints.text = "Points: " + points;
+	}
+	
+	private void _tick()
+	{
+		points++;
+		Invoke ( "_tick", 1f );
+	}
+}
