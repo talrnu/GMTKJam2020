dedfb1ebb55f03954d82d96327f11b9feaa22f99
AlxAlv <alex.alvarado94@outlook.com>
2020-07-11 01:08:49 -0500

    Added primitive tilt mechanic to a plane
    Use WASD to move the plane.
    Literally like 30 lines of code.
    My next TODO's are:
    1) Make it so it doesn't tilt past an angle
    2) Have it tilt back to the normal position when not reciving input.

---
+++ Assets/Scripts/MazeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeController : MonoBehaviour
+{
+    public GameObject target;
+
+    private bool _rotatingForward = false;
+    private bool _rotatingBack = false;
+    private bool _rotatingLeft = false;
+    private bool _rotatingRight = false;
+
+    private const float _rotateSpeed = 80f;
+
+    void Update()
+    {
+        HandleInput();
+
+        transform.RotateAround(target.transform.position, Vector3.forward, (_rotatingForward ? _rotateSpeed : 0) * Time.deltaTime);
+        transform.RotateAround(target.transform.position, Vector3.back, (_rotatingBack ? _rotateSpeed : 0) * Time.deltaTime);
+        transform.RotateAround(target.transform.position, Vector3.left, (_rotatingLeft ? _rotateSpeed : 0) * Time.deltaTime);
+        transform.RotateAround(target.transform.position, Vector3.right, (_rotatingRight ? _rotateSpeed : 0) * Time.deltaTime);
+    }
+
+    void HandleInput()
+    {
+        _rotatingForward = Input.GetKey(KeyCode.W);
+        _rotatingBack = Input.GetKey(KeyCode.S);
+        _rotatingLeft = Input.GetKey(KeyCode.A);
+        _rotatingRight = Input.GetKey(KeyCode.D);
+    }
+}
