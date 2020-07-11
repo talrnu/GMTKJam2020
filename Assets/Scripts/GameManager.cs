8ff308a796dd4f5efa2f546e4afc1d17dd3872bd
mlbarker <marcus.barker@gmail.com>
2020-07-11 03:09:39 -0500

    Added a GameManager game object to restart game via coroutine and hitting the 'R' button when GameOver() method is called.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private float _waitTimeForRestart = 5.0f;
+
+    [SerializeField]
+    private bool _isGameOver = false;
+
+
+    public void GameOver()
+    {
+        _isGameOver = true;
+
+        StartCoroutine(RestartGame());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && _isGameOver)
+        {
+            ReloadScene();
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadSceneAsync(0); // this hard coded value needs to change
+    }
+
+    private IEnumerator RestartGame()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_waitTimeForRestart);
+
+            ReloadScene();
+        }
+    }
+}
