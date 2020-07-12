26ba6860e5979a0d37560f7fd881545fb516b6bd
Archov <scsitransfer@gmail.com>
2020-07-11 19:01:14 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/talrnu/GMTKJam2020""
    This reverts commit 8cbe84565198a83dec8ffad962c093da9f2e4217.

--- Assets/Scripts/LocalPlayer.cs
+++ Assets/Scripts/LocalPlayer.cs
@@ -10,10 +10,16 @@ public class LocalPlayer : MonoBehaviour
         DirectionChoice = null,
         Weight = 1f
     };
+    private Vector2 previousInput = Vector2.zero;
 
     private void Update()
     {
         var input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        if (Mathf.Approximately(input.x, previousInput.x) && Mathf.Approximately(input.y, previousInput.y))
+        {
+            //No change in input, don't waste time processing it again
+            return;
+        }
         
         if (Mathf.Approximately(input.magnitude, 0f))
         {
@@ -37,5 +43,6 @@ public class LocalPlayer : MonoBehaviour
         }
 
         InputVoteCollector.ApplyVote(vote);
+        previousInput = input;
     }
 }
